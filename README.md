ecc9fe21b92dbf34ad09fa64094ffa84db023a55
ouwj <ouwenjie03@163.com>
2016-11-08 00:11:07 +0800

    update readme algorithm v1.1

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # buildPicWithPic
-auto build picture with pictures
+auto build big picture with small pictures.
+
+
+依赖库：
+	pyqt4
+	PIL
+	numpy
+
+main.py是程序入口，运行可看到图形界面。
+buildPicWithPic.py是算法文件，实现了small pictures构成big picture的算法。
+buildPicWidget.py是图形界面文件，实现了main的图形界面。
+buildPicWorkThread.py是界面子线程文件，用以后台运行算法。
+
+建议，small pictures的数量最好超过500张
--- buildPicWithPic.py
+++ buildPicWithPic.py
@@ -12,7 +12,7 @@ import os
 import math
 
 TMP_DIR = 'small_pic_dir/'
-
+MAX_CURRENT_NUM = 15
 
 class BuildPicWithPic:
     def load_picture(self, pic_path, new_w=-1, new_h=-1):
@@ -51,38 +51,40 @@ class BuildPicWithPic:
             (w, h) = im.size
             if w > h:
                 im = im.transpose(Image.ROTATE_90)
-            im = im.resize((30, 40), Image.ANTIALIAS)
+            im = im.resize((90, 120), Image.ANTIALIAS)
             im.save(TMP_DIR+str(idx)+'.jpg', 'jpeg')
             idx += 1
 
-    def find_match_picture(self, patch_arr, dst_arrs):
-        idx = 0
+    def find_match_picture(self, patch_arr, dst_arrs, matchDist):
+        idx = -1
         min_diff = 1e12
-        match_idx = -1
         match_idx = -1
         h, w, c = patch_arr.shape
         patch_arr_1d = patch_arr.reshape((w*h*c))
         for dst_arr in dst_arrs:
+            idx += 1
+            if matchDist[idx] > MAX_CURRENT_NUM:
+                continue
             dst_arr_1d = dst_arr.reshape((w*h*c))
             diff = math.sqrt(sum((patch_arr_1d-dst_arr_1d) ** 2))
             if diff < min_diff:
                 min_diff = diff
                 match_idx = idx
-            idx += 1
+        matchDist[match_idx] += 1
         return match_idx
 
     def build_picture(self, src_arr, dst_arrs):
         h, w, c = src_arr.shape
         result_arr = np.zeros((h, w, c), dtype='int64')
-        patch_w = 30
-        patch_h = 40
+        patch_w = 90
+        patch_h = 120
         len_w = w / patch_w
         len_h = h / patch_h
-
+        matchDist = [0]*len(dst_arrs)
         for i in range(len_h):
             for j in range(len_w):
                 patch_arr = src_arr[i*patch_h:(i+1)*patch_h, j*patch_w:(j+1)*patch_w]
-                match_idx = self.find_match_picture(patch_arr, dst_arrs)
+                match_idx = self.find_match_picture(patch_arr, dst_arrs, matchDist)
                 #print i, j, match_idx
                 result_arr[i*patch_h:(i+1)*patch_h, j*patch_w:(j+1)*patch_w] = dst_arrs[match_idx]
 
@@ -90,9 +92,9 @@ class BuildPicWithPic:
 
     def main(self, src_pic_path, dst_pics_dir):
         print 'src picture loading...'
-        src_arr = self.load_picture(src_pic_path, new_w=3000, new_h=4000)
+        src_arr = self.load_picture(src_pic_path, new_w=9000, new_h=12000)
         print 'preprocessing...'
-        #self.preprocess_dir(dst_pics_dir)
+        self.preprocess_dir(dst_pics_dir)
         dst_arrs = []
         print 'dst picture loading...'
         pic_paths = os.listdir(TMP_DIR)
@@ -101,7 +103,7 @@ class BuildPicWithPic:
             dst_arrs.append(dst_arr)
         print 'picture building...'
         result_arr = self.build_picture(src_arr, dst_arrs)
-        alpha = 0.8
+        alpha = 1
         beta = 1-alpha
         result_arr = alpha * result_arr + beta * src_arr
         print 'picture saving...'
